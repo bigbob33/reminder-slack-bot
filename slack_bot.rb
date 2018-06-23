5a26734abb313353a02bd079acb2a5797ff52c7b
bigbob <gager33@gmail.com>
2018-06-23 16:46:46 +0200

    Push a message to rabbitMQ when slackbot was pinged

--- slack_bot.rb
+++ slack_bot.rb
@@ -1,8 +1,44 @@
 require 'slack-ruby-bot'
+require 'bunny'
+require 'json'
+
+STDOUT.sync = true
+
+class MyView < SlackRubyBot::MVC::View::Base; end
+class MyModel < SlackRubyBot::MVC::Model::Base; end
+
+class MyController < SlackRubyBot::MVC::Controller::Base
+  def ping
+    client.say(channel: data.channel, text: "Wait for it...")
+    set_reminder('pong')
+  end
+
+  private
+
+  def bunny_conn
+    @bunny_conn ||= Bunny.new
+  end
+
+  def set_reminder(message)
+    bunny_conn.start
+    ch = bunny_conn.create_channel
+    q  = ch.queue("slackbot.raw_messages", :auto_delete => true)
+    x  = ch.default_exchange
+
+    x.publish(json_msg(message), :routing_key => q.name)
+  end
+
+  def json_msg(msg)
+    { message: msg }.to_json
+  end
+end
 
 class SlackBot < SlackRubyBot::Bot
-  command 'ping' do |client, data, match|
-    client.say(text: 'pong', channel: data.channel)
+  view = MyView.new
+  model = MyModel.new
+  @controller = MyController.new(model, view)
+  @controller.class.command_class.routes.each do |route|
+    STDERR.puts route.inspect
   end
 end
 
