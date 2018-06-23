5fd2de8123e09ee7a4e05309ac77098ca02a5f85
Łukasz Adamczyk <lukaszadamczyk@rst-it.com>
2018-06-23 19:50:03 +0200

    * Add Interpreter * Chang the adapter so it receives translated messages and posts messages to Slack

--- adapter.rb
+++ adapter.rb
@@ -1,5 +1,6 @@
 require 'slack-ruby-client'
 require 'bunny'
+require 'picky'
 
 Slack.configure do |config|
   config.token = ENV['SLACK_API_TOKEN']
@@ -18,11 +19,11 @@ logger = Logger.new(STDOUT)
 
 begin
   puts ' [*] Waiting for messages. To exit press CTRL+C'
-  queue.subscribe(manual_ack: true, block: true) do |delivery_info, _properties, body|
+  queue.subscribe(block: true) do |_delivery_info, _properties, body|
     logger.info "Incoming message: #{body}"
     parsed_body = JSON.parse(body)
-    client.chat_postMessage(channel: '#general', text: parsed_body['message'], as_user: true)
-    channel.ack(delivery_info.delivery_tag)
+    user = client.users_search(user: parsed_body['user']).members.first
+    client.chat_postMessage(channel: user.id, text: parsed_body['message'], as_user: true)
   end
 rescue Interrupt => _
   connection.close
---
+++ interpreter.rb
@@ -0,0 +1,41 @@
+require 'bunny'
+require 'json'
+
+class Interpreter
+  def initialize
+    @bunny_con = Bunny.new
+    @bunny_con.start
+
+    @channel = @bunny_con.create_channel
+    @input   = @channel.queue('messages.input')
+    @logger  = Logger.new(STDOUT)
+  end
+
+  def run
+    @input.subscribe(block: true) do |_delivery_info, _properties, body|
+      @logger.info "Incoming message: #{body}"
+      parsed_body = JSON.parse(body)
+      # interpreting...
+      publish(parsed_body.to_json, queue: 'messages.output')
+    end
+  rescue Interrupt => _
+    @bunny_con.close
+    exit(0)
+  end
+
+  class << self
+    def run
+      new.run
+    end
+  end
+
+  private
+
+  def publish(message, queue:)
+    @channel.default_exchange.publish(message, routing_key: queue)
+    @logger.info "Outgoing message: #{message}"
+  end
+end
+
+puts ' [*] Waiting for messages. To exit press CTRL+C'
+Interpreter.run
